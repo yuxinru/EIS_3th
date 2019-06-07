a4c85fdc29af43fbb27916c2e57808ce88021de4
seyota <3502886377@qq.com>
2019-06-07 20:52:51 +0800

    123

--- Broker Gateway/src/main/java/com/broker/entity/BuyStopOrder.java
+++
@@ -1,8 +0,0 @@
-package com.broker.entity;
-
-import java.util.LinkedList;
-import java.util.TreeMap;
-
-public class BuyStopOrder {
-    public TreeMap<Integer, LinkedList<Order>> stopOrderMap;
-}
--- Broker Gateway/src/main/java/com/broker/entity/OrderBlotters.java
+++
@@ -1,8 +0,0 @@
-package com.broker.entity;
-
-import java.util.LinkedList;
-import java.util.List;
-
-public class OrderBlotters {
-    List<Orderblotter> orderblotters = new LinkedList<>();
-}
